9e3c88e26417fc257723095308b68ec0c2428c69
moradi <MahvashMoradi925@gmail.com>
2021-08-19 19:45:53 +0430

    search is implemented. User can search with each category. search is work with ajax.

--- SRC/app/pages/urls.py
+++ SRC/app/pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, HomeView, ProductDetailView
+from .views import  ProductDetailView, HomeView
 app_name = 'app.pages'
 urlpatterns = [
-    # path('', home, name='home'),
+    # path('home', home, name='home'),
     path('<int:id>/', HomeView.as_view(), name='home'),
     path('', HomeView.as_view(), name='home'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
+    # path('search/', search_book, name='search'),
 ]
--- SRC/app/pages/views.py
+++ SRC/app/pages/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.views.generic import ListView, DetailView
@@ -9,16 +9,34 @@ from app.book.models import BookModel, CategoryModel
 from app.cart.forms import CartAddProductionForm
 
 
-def home(request):
-    return render(request, 'pages/home.html')
+# def home(request):
+#     return render(request, 'pages/home.html')
 
 
 # class HomeView(ListView):
 #     template_name = 'pages/home.html'
 #     model = BookModel
+# def home(request):
+#     if request.method == 'POST' and request.is_ajax():
+#         print(dict(request.POST.items()))  # محتویات درخواست مشاهده کنید
+#         # print(request.is_ajax())  #print True
+#         input_text = request.POST['inputText']
+#         books = BookModel.objects.all()  # new
+#         if input_text:
+#             books = BookModel.objects.filter(name__contains=input_text)
+#             # message = "HI " + inputText + ' now Time : '+ str(datetime.now())
+#         return JsonResponse({
+#             # 'message':message,
+#             # 'polls':polls   #error
+#             'books': list(books.values())
+#         })
+#     #         product = BookModel.objects.all()
+#
+#     return render(request,'pages/home.html',{})
 
+# return render(request,'pages/home.html',{})
 class HomeView(View):
-    def get(self, request, id=None):
+    def get(self, request, id= None):
         product = BookModel.objects.all()
         # for i in product:
         #     i.cal_discount_price()
@@ -32,6 +50,38 @@ class HomeView(View):
         return render(request, 'pages/home.html',
                       {'product': product, 'categories': categories, 'category': category, 'form': form})
 
+    def post(self, request, id= None):
+        if request.is_ajax():
+            print(dict(request.POST.items()))  # محتویات درخواست مشاهده کنید
+            # print(request.is_ajax())  #print True
+            input_text = request.POST['inputText']
+            # books = BookModel.objects.all()  # new
+            # if input_text:
+            #     books = BookModel.objects.filter(name__contains=input_text)
+            #     if id:
+            #         category = get_object_or_404(CategoryModel, id=id)
+            #         books = BookModel.objects.filter(name__contains=input_text, categories=category)
+            if id:
+                category = get_object_or_404(CategoryModel, id=id)
+                if input_text:
+                    books = BookModel.objects.filter(name__contains=input_text, categories=category)
+                else:
+                    books = BookModel.objects.filter(categories=category)
+            else:
+                if input_text:
+                    books = BookModel.objects.filter(name__contains=input_text)
+                else:
+                    books = BookModel.objects.all()  # new
+
+            # message = "HI " + inputText + ' now Time : '+ str(datetime.now())
+
+            return JsonResponse({
+                # 'message':message,
+                'books': list(books.values())
+            })
+
+        # return render(request,'pages/home.html',{})
+
 
 class ProductDetailView(DetailView):
     model = BookModel
@@ -41,3 +91,21 @@ class ProductDetailView(DetailView):
         context = super(ProductDetailView, self).get_context_data(**kwargs)
         context['form'] = CartAddProductionForm
         return context
+
+#
+# def search_book(request):
+#     if request.method == 'POST' and request.is_ajax():
+#         print(dict(request.POST.items())) # محتویات درخواست مشاهده کنید
+#         # print(request.is_ajax())  #print True
+#         inputText = request.POST['inputText']
+#         books = BookModel.objects.all() #new
+#         if inputText :
+#             books = BookModel.objects.filter(name__contains = inputText)
+#         # message = "HI " + inputText + ' now Time : '+ str(datetime.now())
+#         return JsonResponse({
+#             # 'message':message,
+#             # 'polls':polls   #error
+#             'books':list(books.values())
+#         })
+#
+#     # return render(request,'pages/home.html',{})
